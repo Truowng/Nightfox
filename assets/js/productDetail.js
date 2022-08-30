d39ba9f71e7de84a59e616dc1aa5cc4a389ab55d
DESKTOP-MKH8SQO\quock <quockhanhtruong166@gmail.com>
2022-08-30 20:30:34 +0700

    upload order page

--- assets/js/productDetail.js
+++ assets/js/productDetail.js
@@ -8,9 +8,44 @@ container.addEventListener("wheel", (e) => {
     `translateY(${-((container.scrollLeft += e.deltaY) / 35)}%)`,
     `translateY(${-200 + (container.scrollLeft += e.deltaY) / 35}%)`,
   ];
+
   e.preventDefault();
-  container.scrollLeft += e.deltaY;
+
   separates.forEach((separate, index) => {
     separate.style.transform = scroll[index];
   });
+
+  // document.querySelector(
+  //   ".product-detail-name"
+  // ).style.transform = `translateY(${(container.scrollLeft += e.deltaY) / 10}%)`;
+
+  // document.querySelector(
+  //   ".product-detail-material-image-text span"
+  // ).style.transform = `translateX(${
+  //   25 - (container.scrollLeft += e.deltaY) / 150
+  // }%)`;
+
+  // document.querySelector(
+  //   ".product-detail-material-image-text"
+  // ).style.transform = `translateX(${
+  //   25 - (container.scrollLeft += e.deltaY) / 120
+  // }%)`;
+
+  // document.querySelector(
+  //   ".product-detail-description-content-text"
+  // ).style.transform = `translateX(${
+  //   20 - (container.scrollLeft += e.deltaY) / 55
+  // }%)`;
+
+  // document.querySelector(
+  //   ".product-detail-description-title"
+  // ).style.transform = `translateX(${
+  //   -(container.scrollLeft += e.deltaY) / 150
+  // }%)`;
+
+  // document.querySelector(
+  //   ".product-detail-material"
+  // ).style.transform = `translateX(${
+  //   50 - (container.scrollLeft += e.deltaY) / 60
+  // }%)`;
 });
