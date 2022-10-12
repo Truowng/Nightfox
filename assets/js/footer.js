0955cb081cf3336b8d9ffef96cf2c7c26c8b6339
DESKTOP-MKH8SQO\quock <quockhanhtruong166@gmail.com>
2022-10-12 16:19:09 +0700

    fix source link img footer

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -1,16 +1,10 @@
 const images = document.querySelectorAll(".follow-us-image-container");
 const imageLinks = [
   "../Nightfox/assets/img/HoangAnhModel/1.JPG",
-  "../Nightfox/assets/img/MinhChienModel/3.JPG",
+  "../Nightfox/assets/img/MinhChienModel/3.jpg",
   "../Nightfox/assets/img/HoangAnhModel/2.JPG",
-  "../Nightfox/assets/img/MinhChienModel/4.JPG",
+  "../Nightfox/assets/img/MinhChienModel/4.jpg",
 ];
-// const imageLinks = [
-//   "https://images.unsplash.com/photo-1616837874254-8d5aaa63e273?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=556&q=80",
-//   "https://images.unsplash.com/photo-1582533552406-234434284c17?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=527&q=80",
-//   "https://images.unsplash.com/photo-1524041255072-7da0525d6b34?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80",
-//   "https://images.unsplash.com/photo-1601421415261-a22d30aa0eb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=560&q=80",
-// ];
 
 images.forEach((image, index) => {
   image.style.backgroundImage = `url(${imageLinks[index]})`;
